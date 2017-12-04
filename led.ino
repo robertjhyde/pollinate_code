e9a012a5feda592c03dad7aa021957ca296e3c02
robertjhyde <32895383+robertjhyde@users.noreply.github.com>
2017-12-04 22:59:21 +0000

    Update led.ino

--- led.ino
+++ led.ino
@@ -1,15 +1,13 @@
 #include <FastLED.h>
-int r1 = 0;
-int r2 = 0;
+int r = 0;
 int s = 0;
 
 CRGB bee[3];
 
-CRGB rain1[2];
-CRGB rain2[3];
+CRGB rain[15];
 
-CRGB cloud1[3];
-CRGB cloud2[2];
+//CRGB cloud1[3];
+//CRGB cloud2[2];
 
 CRGB sun[4];
 
@@ -18,13 +16,10 @@ void setup(){
 
   FastLED.addLeds<NEOPIXEL, 1>(bee, 3); //bee front+wing
   
-  FastLED.addLeds<NEOPIXEL, 2>(rain1, 2); //left drop
-  FastLED.addLeds<NEOPIXEL, 3>(rain2, 3); //midleft drop
-  FastLED.addLeds<NEOPIXEL, 4>(rain2, 3); //midright drop
-  FastLED.addLeds<NEOPIXEL, 5>(rain2, 3); //right drop
-  
-  FastLED.addLeds<NEOPIXEL, 6>(cloud1, 3); //bottom cloud
-  FastLED.addLeds<NEOPIXEL, 7>(cloud2, 2); //top cloud
+  FastLED.addLeds<NEOPIXEL, 2>(rain, 15); //rain
+  //FastLED.addLeds<NEOPIXEL, 3>(rain, 3); //midleft drop
+  //FastLED.addLeds<NEOPIXEL, 4>(rain, 3); //midright drop
+  //FastLED.addLeds<NEOPIXEL, 5>(rain, 3); //right drop
   
   FastLED.addLeds<NEOPIXEL, 8>(sun, 4); //sun1
   FastLED.addLeds<NEOPIXEL, 9>(sun, 4); //sun2
@@ -45,20 +40,29 @@ void loop(){
     int com = Serial.read()
     switch (com) {
       case '1';
-        bee[1] = CGRB::Yellow
-        bee[2] = CGRB::Yellow
-        bee[3] = CGRB::Yellow
+        bee[1] = CGRB::Yellow;
+        bee[2] = CGRB::Yellow;
+        bee[3] = CGRB::Yellow;
         FastLED.show();
+        break;
       case '2':
-        rain2[i-1] = CRGB::Black;
-        rain2[i] = CRGB::Blue;
-        r2 = (r2+1)%91;
+        rain[i-1] = CRGB::Black;
+        rain[i] = CRGB::Blue;
+        r = (r+1)%91;
         FastLED.show();
+        break;
       case '3':
         sun[i-1] = CRGB::Black;
-        sun[i] = CRGB::Blue;
+        sun[i] = CRGB(250, 145, 25);
         s = (s+1)%91;
         FastLED.show();
-    
+        break;
+      default:
+        bee[1] = CGRB::Black;
+        bee[2] = CGRB::Black;
+        bee[3] = CGRB::Black;
+        sun[i] = CRGB::Black;
+        rain[i] = CRGB::Black;
+        FastLED.show();
     
 }
