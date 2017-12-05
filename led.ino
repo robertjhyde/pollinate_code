81637d65bb27cf2dc655b050397439f2e81e9c93
robertjhyde <32895383+robertjhyde@users.noreply.github.com>
2017-12-05 14:44:43 +0000

    Update led.ino

--- led.ino
+++ led.ino
@@ -6,25 +6,20 @@ int StateA = 0;
 int StateB = 0;
 int StateC = 0;
 
-CRGB bee[5];
+CRGB bee[4];
 
 CRGB rain[16];
 
-CRGB sun[4];
+CRGB sun[18];
 
 
 void setup(){
 
-  FastLED.addLeds<NEOPIXEL, 1>(bee, 5); //bee front+wing
+  FastLED.addLeds<NEOPIXEL, 1>(bee, 4); //bee front+wing
   
   FastLED.addLeds<NEOPIXEL, 2>(rain, 16); //rain
   
-  FastLED.addLeds<NEOPIXEL, 8>(sun, 4); //sun1
-  FastLED.addLeds<NEOPIXEL, 9>(sun, 3); //sun2
-  FastLED.addLeds<NEOPIXEL, 10>(sun, 3); //sun3
-  FastLED.addLeds<NEOPIXEL, 11>(sun, 3); //sun4
-  FastLED.addLeds<NEOPIXEL, 12>(sun, 3); //sun5
-  FastLED.addLeds<NEOPIXEL, 13>(sun, 3); //sun6
+  FastLED.addLeds<NEOPIXEL, 8>(sun, 18); //sun
 
 }
 
@@ -54,13 +49,11 @@ void loop(){
     bee[1] = CGRB::Yellow;
     bee[2] = CGRB::Yellow;
     bee[3] = CGRB::Yellow;
-    bee[4] = CRGB::Yellow;
     FastLED.show();
   else:
     bee[1] = CGRB::Black;
     bee[2] = CGRB::Black;
     bee[3] = CGRB::Black;
-    bee[4] = CRGB::Black;
     FastLED.show();
   }
     
@@ -75,19 +68,39 @@ void loop(){
   else:
     r1 = 0;
     r2 = 8;
-    for (i=0, i<16, i++); {
+    for (i=0, i<15, i++) {
       rain[i] = CRGB::Black; }
     FastLED.show();
   }
         
   if (StateC == 1) { //write these manually in functions
-    sun[s-1] = CRGB::Black;
-    sun[s] = CRGB(250, 145, 25);
-    s = (s+1)%4;
+    sun[0] = CRGB::Red;
+    if (s = 0) {
+      first_ring();
+    else:
+      second_ring();
+    }
+    s = (s+1)%2;
     FastLED.show();
   else:
-    sun[s] = CRGB::Black;
+    for (i=0, i<17, i++) {
+    sun[i] = CRGB::Black; }
     FastLED.show();
   }
 
+}
+  
+  
+void first_ring() {
+  for (i=1, i<9, i++) {
+    sun[i*2] = CRGB::Black;
+    sun[(i*2)-1] = CRGB::Red;
+  }
+}
+  
+void second_ring() {
+  for (i=1, i<9, i++) {
+    sun[(i*2)-1] = CRGB::Black;
+    sun[i*2] = CRGB::Red;
+  }
 }
