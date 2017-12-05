5a6e5578ce79668a2d5b5cb569a45b0e6199f506
robertjhyde <32895383+robertjhyde@users.noreply.github.com>
2017-12-05 16:25:38 +0000

    Update testled.ino

--- testled.ino
+++ testled.ino
@@ -2,12 +2,13 @@
 int i1 = 0;
 int i2 = 0;
 int i3 = 8;
+int s = 0;
 
-CRGB leds[7];
+CRGB sun[18];
 CRGB leds2[16];
 
 void setup() {
-  FastLED.addLeds<NEOPIXEL, 1>(leds, 7);
+  FastLED.addLeds<NEOPIXEL, 1>(sun, 18);
   FastLED.addLeds<NEOPIXEL, 2>(leds2, 16);
   Serial.begin(9600);
 }
@@ -17,12 +18,13 @@ void loop() {
     int inp = Serial.read();
     switch (inp) {
       case '1':
-        leds[i1-1] = CRGB::Black;
-        leds[i1] = CRGB::Red;
-        leds2[i2-1] = CRGB::Black;
-        leds2[i2] = CRGB::Orange;
-        i1 = (i1+1)%7;
-        i2 = (i2+1)%16;
+        sun[2] = CRGB::Red;
+        if (s = 0) {
+          first_ring(); }
+        else {
+          second_ring();
+        }
+        s = (s+1)%2;
         FastLED.show();
         break;
       case '2':   
@@ -42,5 +44,46 @@ void loop() {
         leds2[i2] = CRGB::Black;
         FastLED.show();
     }
+  }
+}
+
+
+void first_ring(){
+  sun[1] = CRGB::Red;
+  sun[3] = CRGB::Red;
+  sun[6] = CRGB::Red;
+  sun[7] = CRGB::Red;
+  sun[10] = CRGB::Red;
+  sun[12] = CRGB::Red;
+  sun[13] = CRGB::Red;
+  sun[16] = CRGB::Red;
+  sun[0] = CRGB::Black;
+  sun[4] = CRGB::Black;
+  sun[7] = CRGB::Black;
+  sun[8] = CRGB::Black;
+  sun[9] = CRGB::Black;
+  sun[11] = CRGB::Black;
+  sun[14] = CRGB::Black;
+  sun[15] = CRGB::Black;
+  }
+}
+  
+void second_ring(){
+  sun[0] = CRGB::Red;
+  sun[4] = CRGB::Red;
+  sun[7] = CRGB::Red;
+  sun[8] = CRGB::Red;
+  sun[9] = CRGB::Red;
+  sun[11] = CRGB::Red;
+  sun[14] = CRGB::Red;
+  sun[15] = CRGB::Red;
+  sun[1] = CRGB::Black;
+  sun[3] = CRGB::Black;
+  sun[6] = CRGB::Black;
+  sun[7] = CRGB::Black;
+  sun[10] = CRGB::Black;
+  sun[12] = CRGB::Black;
+  sun[13] = CRGB::Black;
+  sun[16] = CRGB::Black;
   }
 }
