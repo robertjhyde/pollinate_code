ea3e8a79c5e32ce23215628f9b5371b53eed5391
robertjhyde <32895383+robertjhyde@users.noreply.github.com>
2017-12-07 15:08:33 +0000

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -25,27 +25,40 @@ count = 0
 
 def watch_bee():
     global shutdown, solved_bee
+    bb_on = False
     while not shutdown:
-        if beebutton.is_pressed:
+        if beebutton.is_pressed and bb_on == False:
             solved_bee = (solved_bee+1)%2
             ser.write('1')
+            bb_on == True
+        if not beebutton.is_pressed and bb_on == True:
+            bb_on == False
     
 def watch_rain():
     global shutdown, solved_rain, count, falling
+    lg_on = False
     while not shutdown:
         while count < 5 and not shutdown:
-            if GPIO.input(17) == 1:
+            if GPIO.input(17) == 1 and lg_on == False:
+                lg_on = True
                 count += 1
+            if GPIO.input(17) == 1 and lg_on == True:
+                lg_on = False
         solved_rain = (solved_rain+1)%2
         ser.write('2') #turns the rain and clouds on
         count = 0
 
 def watch_sun():
     global shutdown, solved_sun
+    sb_on == False
     while not shutdown:
-        if sunbutton.is_pressed:
+        if sunbutton.is_pressed and sb_on == False:
+            sb_on = True
             solved_sun = (solved_sun+1)%2
             ser.write('3')
+        if not sunbutton.is_pressed and sb_on == True:
+            sb = False
+            
     
 def flower():
     servo.min()
